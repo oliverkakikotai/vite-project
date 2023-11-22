114af35e06e09bfb7600cecff3f040d60beb2662
zhangyuzhuo <23aw0120@jec.ac.jp>
2023-11-22 15:56:41 +0900

    1-1st

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+//test github
 function App() {
   const [task, setTask] = useState([]); //タスク全体のリストを管理するステート
   const [newTask, setNewTask] = useState(""); //新しいタスクを追加するときのステート
